4ded63d9f5bbcbab6930d5393b2151a465d4d327
lette1394 <lette1394+github@gmail.com>
2017-12-31 19:49:16 +0900

    Initial commit

---
+++ src/main/java/Driver.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Driver {
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(car.getTireBrand());
+    }
+}
---
+++ src/main/java/Tire.java
@@ -0,0 +1,5 @@
+package main.java;
+
+interface Tire {
+    String getBrand();
+}
