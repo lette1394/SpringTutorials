449eb4897fbab88b073535c5904f36c13dfe8a84
lette1394 <lette1394+github@gmail.com>
2017-12-31 20:40:01 +0900

    스프링을 통한 의존성 주입 1 - XML 파일 사용

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,9 +1,14 @@
 package main.java;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Driver {
     public static void main(String[] args) {
-        Tire tire = new KoreaTire();
-        Car car = new Car();
+        ApplicationContext context = new ClassPathXmlApplicationContext("context002.xml");
+
+        Tire tire = context.getBean("koreaTire", Tire.class);
+        Car car = context.getBean("car", Car.class);
         car.setTire(tire);
 
         System.out.println(car.getTireBrand());
