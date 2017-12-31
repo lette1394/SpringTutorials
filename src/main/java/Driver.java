9c117bc8b66e59e147c1ffc254daf076811eb43d
lette1394 <lette1394+github@gmail.com>
2017-12-31 20:58:37 +0900

    스프링을 통한 의존성 주입 2 - XML 파일에서 속성 주입

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,11 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Driver {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("context002.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("/context002.xml");
 
-        Tire tire = context.getBean("koreaTire", Tire.class);
         Car car = context.getBean("car", Car.class);
-        car.setTire(tire);
 
         System.out.println(car.getTireBrand());
     }
--- test/main/java/CarTest.java
+++ test/main/java/CarTest.java
@@ -2,24 +2,29 @@ package main.java;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/context002.xml")
 public class CarTest {
+
+    @Autowired
+    Car carKorea;
+
+    @Autowired
+    Car carAmerica;
+
     @Test
     public void 코리아타이어_브랜드() {
-        Tire tire = new KoreaTire();
-        Car car = new Car();
-        car.setTire(tire);
-
-        assertEquals("장착된 타이어 = 코리아 타이어", car.getTireBrand());
+        assertEquals("장착된 타이어 = 코리아 타이어", carKorea.getTireBrand());
     }
 
     @Test
     public void 미국타이어_브랜드() {
-        Tire tire = new AmericaTire();
-        Car car = new Car();
-        car.setTire(tire);
-
-        assertEquals("장착된 타이어 = 미국 타이어", car.getTireBrand());
+        assertEquals("장착된 타이어 = 미국 타이어", carAmerica.getTireBrand());
     }
 }
